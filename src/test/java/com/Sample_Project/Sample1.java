ef69fb0c19ee02d382fb766da1a757c9205490cf
snegapriya <snegapriya20@gmail.com>
2022-02-12 08:42:50 +0530

    Java Concepts Completed

---
+++ src/test/java/com/Sample_Project/Sample1.java
@@ -0,0 +1,14 @@
+package com.Sample_Project;
+
+public class Sample1 {
+
+	
+	public Sample1() {
+		
+	}
+	
+	public void demo() {
+
+		System.out.println("Demo");
+	}
+}
---
+++ src/test/java/com/Sample_Project/Sample2.java
@@ -0,0 +1,13 @@
+package com.Sample_Project;
+
+public class Sample2 {
+
+	
+	public static void main(String[] args) {
+		
+		Sample1 s1 = new Sample1();
+		s1.demo();
+		
+		
+	}
+}
